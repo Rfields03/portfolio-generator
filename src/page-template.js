ba0fd9824d18fefeb9577929b99148b9f4d62b42
Robert Field <rfields03@gmail.com>
2022-01-18 09:58:23 -0500

    update to files

--- src/page-template.js
+++ src/page-template.js
@@ -14,14 +14,119 @@ const generateAbout = aboutText => {
 
 const generateProjects = projectsArr => {
   return `
-    <section class="my-3" id="portfolio">
+  <section class="my-3" id="portfolio">
     <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-    <div class="flex-row justify-space-beteen">
-      <!-- Leaving this empty as we'll dynamically insert project HTML here -->
+    <div class="flex-row justify-space-between">
+    ${projectsArr
+    .filter(({ feature }) => feature)
+    .map(({ name, description, languages, link }) => {
+      return `
+      <div class="col-12 mb-2 bg-dark text-light p-3">
+      <h3 class="portfolio-item-title text-light">${name}</h3>
+      <h5 class="portfolio-languages">
+        Built With:
+        ${languages.join(', ')}
+      </h5>
+      <p>${description}</p>
+      <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+    </div>
+  `;
+  })
+  .join('')}
+
+  ${projectsArr
+    .filter(({ feature }) => !feature)
+    .map(({ name, description, languages, link }) => {
+      return `
+      <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+        <h3 class="portfolio-item-title text-light">${name}</h3>
+        <h5 class="portfolio-languages">
+          Built With:
+          ${languages.join(', ')}
+        </h5>
+        <p>${description}</p>
+        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+      </div>
+    `;
+    })
+    .join('')}
+  </div>
+</section>
+`;
+};
+
+  // get array of just featured projects
+  const featuredProjecs = projectHtmlArr.filter(project => {
+    if (project.feature) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  // get array of all non-featured proejcts
+  const nonFeaturedProjects = projectHtmlArr.filter(project => {
+    if (!project.feature) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
+    return `
+      <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
+        <h3 class="portfolio-item-title text-light">${name}</h3>
+        <h5 class="portfolio-languages">
+          Built With:
+          ${languages.join(', ')}
+        </h5>
+        <p>${description}</p>
+        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+      </div>
+    `;
+  });
+
+  const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
+    return `
+      <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
+        <h3 class="portfolio-item-title text-light">${name}</h3>
+        <h5 class="portfolio-languages">
+          Built With:
+          ${languages.join(', ')}
+        </h5>
+        <p>${description}</p>
+        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+      </div>
+    `;
+  });
+
+  const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
+    ({ name, description, languages, link }) => {
+      return `
+        <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+          <h3 class="portfolio-item-title text-light">${name}</h3>
+          <h5 class="portfolio-languages">
+            Built With:
+            ${languages.join(', ')}
+          </h5>
+          <p>${description}</p>
+          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        </div>
+      `;
+    }
+  );
+
+  return `
+  <section class="my-3" id="portfolio">
+    <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+    <div class="flex-row justify-space-between">
+    ${featuredProjectHtmlArr.join('')}
+    ${nonFeaturedProjectHtmlArr.join('')}
     </div>
   </section>
-  `;
-}
+`;
+
 
 module.exports = templateData => {
   console.log(templateData);
