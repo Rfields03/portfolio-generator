1a5f5426889477d34cb15740b31b10f5acb4bd68
Robert Field <rfields03@gmail.com>
2022-01-18 12:25:46 -0500

    repair folders

--- src/page-template.js
+++ src/page-template.js
@@ -5,137 +5,65 @@ const generateAbout = aboutText => {
   }
 
   return `
-  <section class="my-3" id="about">
-    <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-    <p>${aboutText}</p>
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
     </section>
-    `;
+  `;
 };
 
+// create the projects section
 const generateProjects = projectsArr => {
   return `
-  <section class="my-3" id="portfolio">
-    <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-    <div class="flex-row justify-space-between">
-    ${projectsArr
-    .filter(({ feature }) => feature)
-    .map(({ name, description, languages, link }) => {
-      return `
-      <div class="col-12 mb-2 bg-dark text-light p-3">
-      <h3 class="portfolio-item-title text-light">${name}</h3>
-      <h5 class="portfolio-languages">
-        Built With:
-        ${languages.join(', ')}
-      </h5>
-      <p>${description}</p>
-      <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-    </div>
-  `;
-  })
-  .join('')}
+    <section class="my-3" id="portfolio">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+      <div class="flex-row justify-space-between">
+      ${projectsArr
+        .filter(({ feature }) => feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 mb-2 bg-dark text-light p-3">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.map(language => language).join(',')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+        })
+        .join('')}
 
-  ${projectsArr
-    .filter(({ feature }) => !feature)
-    .map(({ name, description, languages, link }) => {
-      return `
-      <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-        <h3 class="portfolio-item-title text-light">${name}</h3>
-        <h5 class="portfolio-languages">
-          Built With:
-          ${languages.join(', ')}
-        </h5>
-        <p>${description}</p>
-        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+      ${projectsArr
+        .filter(({ feature }) => !feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+        })
+        .join('')}
       </div>
-    `;
-    })
-    .join('')}
-  </div>
-</section>
-`;
+    </section>
+  `;
 };
 
-  // get array of just featured projects
-  const featuredProjecs = projectHtmlArr.filter(project => {
-    if (project.feature) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-
-  // get array of all non-featured proejcts
-  const nonFeaturedProjects = projectHtmlArr.filter(project => {
-    if (!project.feature) {
-      return true;
-    } else {
-      return false;
-    }
-  });
-
-  const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
-    return `
-      <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-        <h3 class="portfolio-item-title text-light">${name}</h3>
-        <h5 class="portfolio-languages">
-          Built With:
-          ${languages.join(', ')}
-        </h5>
-        <p>${description}</p>
-        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-      </div>
-    `;
-  });
-
-  const featuredProjectHtmlArr = featuredProjects.map(({ name, description, languages, link }) => {
-    return `
-      <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-        <h3 class="portfolio-item-title text-light">${name}</h3>
-        <h5 class="portfolio-languages">
-          Built With:
-          ${languages.join(', ')}
-        </h5>
-        <p>${description}</p>
-        <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-      </div>
-    `;
-  });
-
-  const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
-    ({ name, description, languages, link }) => {
-      return `
-        <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-          <h3 class="portfolio-item-title text-light">${name}</h3>
-          <h5 class="portfolio-languages">
-            Built With:
-            ${languages.join(', ')}
-          </h5>
-          <p>${description}</p>
-          <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-        </div>
-      `;
-    }
-  );
-
-  return `
-  <section class="my-3" id="portfolio">
-    <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-    <div class="flex-row justify-space-between">
-    ${featuredProjectHtmlArr.join('')}
-    ${nonFeaturedProjectHtmlArr.join('')}
-    </div>
-  </section>
-`;
-
-
+// export function to generate entire page
 module.exports = templateData => {
-  console.log(templateData);
-  // destructure page data by section const { projects, about, ...header } = templateData;
+  // destructure page data by section
+  const { projects, about, ...header } = templateData;
 
   return `
   <!DOCTYPE html>
   <html lang="en">
-  
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -152,17 +80,17 @@ module.exports = templateData => {
         <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
         <nav class="flex-row">
           <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-        header.github
+            header.github
           }">GitHub</a>
         </nav>
       </div>
-  </header>
-  <main class="container my-5">
-    ${generateAbout(about)}
-    ${generateProjects(projects)}
-  </main>
-  <footer class="container text-center py-3">
-    <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+    </header>
+    <main class="container my-5">
+      ${generateAbout(about)}
+      ${generateProjects(projects)}
+    </main>
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
     </footer>
   </body>
   </html>
